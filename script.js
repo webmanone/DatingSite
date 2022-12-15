86e896e9a7b2fb8a8d512de03c71d3f064cbffb1
LukeM <mansfieldluke98@gmail.com>
2022-12-15 19:36:55 +0000

    changed div to hidden, added js code to make left and right buttons move the div left and right

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function goHome(){
 document.location="index.html"
 } */
 
+const leftButton=document.getElementById('leftButton');
+const rightButton=document.getElementById('rightButton');
+
+leftButton.onclick = function () {
+    document.getElementById('interest1').scrollLeft -= 20;
+}
+
+rightButton.onclick = function () {
+    document.getElementById('interest1').scrollLeft += 20;
+}
