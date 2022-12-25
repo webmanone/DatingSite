0436ca31b6b8e93a5bd414e10ae997b09de92119
LukeM <mansfieldluke98@gmail.com>
2022-12-25 12:25:35 +0000

    changed styling of sign up a bit more

--- signup.php
+++ signup.php
@@ -7,7 +7,7 @@
     <form action="signupInc.php" method="post">
         <input type="text" name="firstname" placeholder="First name...">
         <input type="text" name="lastname" placeholder="Last name...">
-        <input type="date" name="birthday" placeholder="Date of birth...">
+        <div><input type="date" name="birthday" placeholder="Date of birth:"></div>
         <select name="gender">
             <option value="" disabled selected>Gender...</option>
             <option value="male">Male</option>
@@ -23,9 +23,9 @@
             <option value="bisexual">Bisexual</option>
             <option value="other">Other</option>
         </select>
-        <input type="text" name="email" placeholder="Email...">
-        <input type="password" name="pwd" placeholder="Password...">
-        <input type="password" name="pwdrepeat" placeholder="Repeat password...">
+        <div><input type="text" name="email" placeholder="Email..."></div>
+        <div><input type="password" name="pwd" placeholder="Password..."></div>
+        <div><input type="password" name="pwdrepeat" placeholder="Repeat password..."></div>
         <div><button type="submit" name="submit">Sign Up</button></div>
     </form>
     </div>
