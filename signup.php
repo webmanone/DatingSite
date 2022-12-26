df0d6d54fb607dd4b50b3c3519eff1ae9a11c24b
LukeM <mansfieldluke98@gmail.com>
2022-12-26 12:16:35 +0000

    edited signup form to add some fields and changed the layout

--- signup.php
+++ signup.php
@@ -7,8 +7,7 @@
     <form action="signupInc.php" method="post">
         <input type="text" name="firstname" placeholder="First name...">
         <input type="text" name="lastname" placeholder="Last name...">
-        <div><input type="date" name="birthday" placeholder="Date of birth:"></div>
-        <select name="gender">
+        <div><select name="gender">
             <option value="" disabled selected>Gender...</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
@@ -22,10 +21,13 @@
             <option value="gaylesbian">Gay/Lesbian</option>
             <option value="bisexual">Bisexual</option>
             <option value="other">Other</option>
-        </select>
-        <div><input type="text" name="email" placeholder="Email..."></div>
-        <div><input type="password" name="pwd" placeholder="Password..."></div>
-        <div><input type="password" name="pwdrepeat" placeholder="Repeat password..."></div>
+        </select></div>
+        <div><input type="date" name="birthday" placeholder="Date of birth:">
+        <input type="text" name="location" placeholder="Location..."></div>
+        <div><input type="text" name="email" placeholder="Email...">
+            <input type="tel" name="phone" placeholder="Phone Number..." pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required></div>
+        <div><input type="password" name="pwd" placeholder="Password...">
+             <input type="password" name="pwdrepeat" placeholder="Repeat password..."></div>
         <div><button type="submit" name="submit">Sign Up</button></div>
     </form>
     </div>
