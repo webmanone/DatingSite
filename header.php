5dc1b7d4b0996c466806f60805d8fe9fb7702ccc
LukeM <mansfieldluke98@gmail.com>
2022-12-27 11:45:58 +0000

    added activePage js file to dynamically add a css class to whichever page is active

--- header.php
+++ header.php
@@ -4,17 +4,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="style.css">
+    <script defer src="activePage.js"></script>
     <title>Dating Site</title>
 </head>
 <body>
 
-<div>
-    <ul class="navbar">
+<nav class="navbar">
+    <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="myProfile.php">My Profile</a></li>
         <li><a href="login.php">Log in</a></li>
         <li><a href="signup.php">Sign up</a></li>
     </ul>
-</div>    
+</nav>    
 
 <div>
