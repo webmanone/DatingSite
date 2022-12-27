2ff1936a0b05beaba7ad6af160b31637f4765d8b
LukeM <mansfieldluke98@gmail.com>
2022-12-27 12:02:03 +0000

    added login include file and edited login page so that only email and password are there

--- login.php
+++ login.php
@@ -2,42 +2,17 @@
     include_once 'header.php';
 ?>
     <section class="signupForm">
-        <h2 class="pad">Sign Up</h2>
+        <h2 class="pad">Log In</h2>
         <div class="signupFormInner">
-            <form action="signupInc.php" method="post">
-                <input type="text" name="firstname" placeholder="First name...">
-                <input type="text" name="lastname" placeholder="Last name...">
+            <form action="loginInc.php" method="post">
                 <div>
-                    <select name="gender">
-                        <option value="" disabled selected>Gender...</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="transmale">Trans Male</option>
-                        <option value="transfemale">Trans Female</option>
-                        <option value="nonbinary">Non-Binary</option>
-                    </select>
-                    <select name="orientation">
-                        <option value="" disabled selected>Orientation...</option>    
-                        <option value="straight">Straight</option>
-                        <option value="gaylesbian">Gay/Lesbian</option>
-                        <option value="bisexual">Bisexual</option>
-                        <option value="other">Other</option>
-                    </select>
-                </div>
-                <div>
-                    <input type="date" name="birthday" placeholder="Date of birth:">
-                    <input type="text" name="location" placeholder="Location...">
-                </div>
-                <div>
-                    <input type="text" name="email" placeholder="Email...">
-                    <input type="tel" name="phone" placeholder="Phone Number..." pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+                    <input type="text" name="emailOrNumber" placeholder="Email/Phone Number...">
                 </div>
                 <div>
                     <input type="password" name="pwd" placeholder="Password...">
-                    <input type="password" name="pwdrepeat" placeholder="Repeat password...">
                 </div>
                 <div>
-                    <button type="submit" name="submit">Sign Up</button>
+                    <button type="submit" name="submit">Log In</button>
                 </div>
             </form>
         </div>
