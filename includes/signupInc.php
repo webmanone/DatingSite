2e291335e354d5fbef23ae600388c6aec0b9baf8
LukeM <mansfieldluke98@gmail.com>
2022-12-29 16:17:28 +0000

    added some error validation on signup inc to catch if user is already in database etc.

--- includes/signupInc.php
+++ includes/signupInc.php
@@ -12,7 +12,44 @@ if (isset($_POST["submit"])) {
     $phone = $_POST["phone"];
     $pwd = $_POST["pwd"];
     $pwdRepeat = $_POST["pwdrepeat"];
+
+    require_once 'dbhInc.php';
+    require_once 'functionsInc.php';
+
+    if (emptyInputSignup($firstName, $lastName, $gender, $orientation, $birthday, $location, $email, $phone, $pwd, $pwdRepeat) !== false) {
+        header("location: ../signup.php?error=emptyinput");  
+        exit();  
+    }
+
+    if (invalidEmail($email) !== false) {
+        header("location: ../signup.php?error=invalidemail");  
+        exit();  
+    }
+
+    if (invalidNumber($phone) !== false) {
+        header("location: ../signup.php?error=invalidnumber");  
+        exit();  
+    }
+
+    if (pwdMatch($pwd, $pwdRepeat) !== false) {
+        header("location: ../signup.php?error=invalidpasswords");  
+        exit();  
+    }
+
+    if (emailExists($conn, $email) !== false) {
+        header("location: ../signup.php?error=emailtaken");  
+        exit();  
+    }
+
+    if (numberExists($conn, $phone) !== false) {
+        header("location: ../signup.php?error=numbertaken");  
+        exit();  
+    }
+
+    createUser($conn, $firstName, $lastName, $gender, $orientation, $birthday, $location, $email, $phone, $pwd);
+
 }
 else {
     header("location: ../signup.php");
+    exit();
 }
