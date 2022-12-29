d9260cd17a0e6c280e511286bdf3d8ae91eadcbd
LukeM <mansfieldluke98@gmail.com>
2022-12-29 13:03:46 +0000

    populated signupinc with the data that the user will enter

--- includes/signupInc.php
+++ includes/signupInc.php
@@ -1,7 +1,17 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    echo "It works";
+    
+    $firstName = $_POST["firstname"];
+    $lastName = $_POST["lastname"];
+    $gender = $_POST["gender"];
+    $orientation = $_POST["orientation"];
+    $birthday = $_POST["birthday"];
+    $location = $_POST["location"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
 }
 else {
     header("location: ../signup.php");
