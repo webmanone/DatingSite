bb69104406b0a6a9481a73d76f89ef3349c4390c
LukeM <mansfieldluke98@gmail.com>
2022-12-30 14:45:42 +0000

    error building trees trying to fix

--- includes/functionsInc.php
+++ includes/functionsInc.php
@@ -1,1 +1,23 @@
 <?php
+
+function emptyInputSignup($firstName, $lastName, $gender, $orientation, $birthday, $location, $email, $phone, $pwd, $pwdRepeat) {
+    $result;
+    if (empty($firstName) || empty($lastName) || empty($gender) || empty($orientation) || empty($birthday) || empty($location) || empty($email) || empty($phone) || empty($pwd) || empty($pwdRepeat)){
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+
+function invalidEmail($email) {
+    $result;
+    if (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/i"), $email){
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
