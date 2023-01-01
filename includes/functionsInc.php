11b6b9a6c63c582c9f9e73480ce2ec8de4e6260a
LukeM <mansfieldluke98@gmail.com>
2023-01-01 12:36:59 +0000

    changed number validation to have 11 numbers for the moment instead of 10

--- includes/functionsInc.php
+++ includes/functionsInc.php
@@ -24,7 +24,7 @@ function invalidEmail($email) {
 
 function invalidNumber($phone) {
     $result;
-    if (!strlen($phone) === 10){
+    if (!strlen($phone) === 11){
         $result = true;
     }
     else {
