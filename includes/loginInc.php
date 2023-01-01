6c9b51fa3ada5b8e4e357b7907ec22f1866b44a2
LukeM <mansfieldluke98@gmail.com>
2023-01-01 13:37:09 +0000

    started working on login backend, making sure the user has to submit data to access an account, adding a impty input validation and then logging in the user once everything is checked.

---
+++ includes/loginInc.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST["submit"])) {
+
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+
+    require_once 'dbhInc.php';
+    require_once 'functionsInc.php';
+
+    if (emptyInputLogin($username, $pwd) !== false) {
+        header("location: ../login.php?error=emptyinput");  
+        exit();  
+    }
+
+    loginUser($conn, $username, $pwd);
+}
+else {
+    header("location: ../login.php");
+    exit();
+}
