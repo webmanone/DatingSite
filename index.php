ddb16a0112add8723ef9a155f4b6e688dec707dd
LukeM <mansfieldluke98@gmail.com>
2023-01-02 14:34:24 +0000

    added basic login message for when you log in

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
     <br><br><br><br><br><br>
     
     <h1>Dating Site</h1>
-
+    
+    <section>
+        <?php
+            if (isset($_SESSION["userid"])) {
+                echo "<p>Welcome, " . $_SESSION["userFirstname"] . "!</p>";
+                
+            }
+        ?>
+    </section>
 <?php
     include_once 'footer.php';
 ?>
