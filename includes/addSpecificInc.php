fdf6fca974f95d7dd26fb1c6a18d4c495ffb362c
LukeM <mansfieldluke98@gmail.com>
2023-01-28 14:17:31 +0000

    added new php file which will add a new row in the items database for each specific interest added, started working on the function but still need some work

---
+++ includes/addSpecificInc.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/datingSite/header.php';
+
+if(isset($_POST["specificSubmit"])) {
+
+    //$interestsId = get interestsId in here
+    $userId = $_SESSION['userid'];
+    //$itemsTitle = get category in here
+    $itemsName = $_POST["specificInterest"];
+
+    require_once 'dbhInc.php';
+    require_once 'functionsInc.php';
+
+    function addSpecific ($conn, $interestsId, $userId, $itemsTitle, $itemsName) {
+        $sql = "INSERT INTO items (interestsId, usersId, itemsTitle, itemsName) VALUES (?, ?, ?, ?);";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("location: ../userProfile.php?error=stmtfailed");  
+            exit(); 
+        }else {
+            mysqli_stmt_bind_param($stmt, "iiss", $interestsId, $userId, $itemsTitle, $itemsName);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+            header("location: ../userProfile.php?error=none");  
+            exit();
+        }
+    }
+}
+
