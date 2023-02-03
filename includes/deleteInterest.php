ba546aac7500382c9eba9a7883a413053209a84e
LukeM <mansfieldluke98@gmail.com>
2023-02-03 18:47:16 +0000

    discovered error when deleting the first category in the interests list. commented to come back to next time

--- includes/deleteInterest.php
+++ includes/deleteInterest.php
@@ -5,7 +5,7 @@ require_once 'dbhInc.php';
 if(isset($_GET['interestsId'])){
     $interestsId = $_GET['interestsId'];
     $sql = "DELETE FROM interests WHERE interestsId = '$interestsId';";
-    $result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);//fatal error here when deleting the first item in the interest category list, all others delete fine
 
     if ($result) {
         mysqli_close($conn);
